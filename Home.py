37882c00a3013c169176e4abcae679bcc3d3145b
greyscale <grey.liedtke@scaled-power.com>
2023-04-21 17:16:59 -0700

    great

--- Home.py
+++ Home.py
@@ -1,3 +1,9 @@
+"""
+streamlit run Home.py
+"""
+
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
--- pages/WikEq.py
+++ pages/WikEq.py
@@ -8,6 +8,12 @@ from WikEq.DF.dfs import df_eq, df_eqv, df_eqd
 st.title('WikEq')
 query_params = st.experimental_get_query_params()
 
+with st.expander("Equation Categories"):
+    eq_cats = sorted(df_eq["EQ_CAT"].unique())
+    for e in eq_cats:
+        st.markdown(f"[{e}](/WikEq?Category={e})")
+
+
 if "Equation" in query_params:
     
     eq_name = query_params["Equation"][0]
@@ -17,19 +23,26 @@ if "Equation" in query_params:
     st.markdown(f"$$ {eqj['Eqn']} $$")
     st.markdown(eqj['Short_Desc'])
     st.markdown(f"- [Wikipedia]({eqj['WIKI_LINK']})")
-    st.markdown(f"- [{eqj['EQ_CAT']} equations](/WikEq/Categories/{eqj['EQ_CAT']})")
+    st.markdown(f"- [{eqj['EQ_CAT']} equations](/WikEq?Category={eqj['EQ_CAT']})")
 
     
     eq_vars = df_eqv[df_eqv['EQ_id']==int(eq['EQ_id'])].sort_values(by=['Symbol'])
-    eqv = sorted(eq_vars['Symbol'].unique())
+    
+    v_table = eq_vars[['Symbol', 'Short_Desc', 'SI_Units']]
+    eq_vars
+    v_table = v_table.set_index('Symbol')
+
+    
     st.title("Equation Variables")
-    st.dataframe(eq_vars)
+    st.dataframe(v_table)
+    print('ok')
 
-    st.title("Symbolic Solving")
-    st.markdown(f"$$ {eq_vars.iloc[0]['Solved']} $$")
+    # st.title("Symbolic Solving")
+    # st.markdown(f"$$ {eq_vars.iloc[0]['Solved']} $$")
 
-    eqv_d = eq_vars.to_dict('records')
-    dim_links = sorted(eq_vars['D_Name'].unique())
+    # eqv = sorted(eq_vars['Symbol'].unique())
+    # eqv_d = eq_vars.to_dict('records')
+    # dim_links = sorted(eq_vars['D_Name'].unique())
 
 if "Category" in query_params:
     category = query_params["Category"][0]
@@ -38,15 +51,25 @@ if "Category" in query_params:
 
     eqns = df_eq.loc[df_eq["EQ_CAT"] == category]
     eqns = eqns[["Name", "Eqn"]].sort_values(by="Name")
+    eqns_d = eqns.to_dict(orient='records')
+    cols = st.columns(2)
+    for e in eqns_d:
+        eq_name = e['Name']
+        link = eq_name.replace(" ", "%20")
+        with cols[0]:
+            st.markdown(f"[{eq_name}](/WikEq?Equation={link})")
+        with cols[1]:
+            st.markdown(f"$$ {e['Eqn']} $$")
+
+    st.dataframe(eqns)
+
+
     for i,row in eqns.iterrows():
         st.markdown(f"[{row['Name']}](/WikEq?Equations={row['Name']}) : $$ {row['Eqn']} $$")
 
     
-    st.title("Equation Categoreis")
+# st.title("Equation Categoreis")
 
-eq_cats = sorted(df_eq["EQ_CAT"].unique())
-for e in eq_cats:
-    st.markdown(f"[{e}](/WikEq?Category={e})")
 
 
 

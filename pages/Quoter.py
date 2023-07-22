27f59174c0dde38c86c303850ce25522ab7e2f6f
Grey <grey.liedtke@gmail.com>
2023-07-21 21:19:07 -0700

    with instructions

--- pages/Quoter.py
+++ pages/Quoter.py
@@ -5,12 +5,13 @@ from streamlit_gsheets import GSheetsConnection
 
 st.markdown("# Quotes")
 
-st.markdown("[Contribute](https://forms.gle/H6aVkxk1ntaRAiRX7) to the log! Allow several minutes to update")
+st.markdown(
+    "[Contribute](https://forms.gle/H6aVkxk1ntaRAiRX7) to the log! Allow several minutes to update"
+)
 
 url = "https://docs.google.com/spreadsheets/d/1z49yYw1XBOccYkBhr0ZHTdY-YDqOBZMh7ci2bJTh7fI/edit?usp=sharing"
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-# https://docs.google.com/spreadsheets/d/1z49yYw1XBOccYkBhr0ZHTdY-YDqOBZMh7ci2bJTh7fI/edit?usp=sharing
-quotes = conn.read(spreadsheet=url, usecols=[1,2, 3])
+quotes = conn.read(spreadsheet=url, usecols=[1, 2, 3])
 
 st.divider()
 for i, q in quotes.iterrows():
@@ -20,3 +21,12 @@ for i, q in quotes.iterrows():
 
 with st.expander("QuoTable"):
     st.dataframe(quotes)
+
+with st.expander("GSheets Experimental Connection Instructions"):
+    st.code("""
+            # only 3 lines of code!!
+        url = "https://docs.google.com/spreadsheets/d/1z49yYw1XBOccYkBhr0ZHTdY-YDqOBZMh7ci2bJTh7fI/edit?usp=sharing"
+        conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+        quotes = conn.read(spreadsheet=url, usecols=[1, 2, 3])
+        """, language="python"
+    )
